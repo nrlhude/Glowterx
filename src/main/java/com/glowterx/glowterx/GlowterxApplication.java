928624b0aa9ea9d2af02888b54925d7039699297
Ieskandar Zulqarnain <73181414+Zieszx@users.noreply.github.com>
2023-01-11 23:44:13 +0800

    Add files via upload

---
+++ src/main/java/com/glowterx/glowterx/GlowterxApplication.java
@@ -0,0 +1,13 @@
+package com.glowterx.glowterx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowterxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlowterxApplication.class, args);
+	}
+
+}
