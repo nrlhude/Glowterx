240a4ce94be756c9c70166a6e62f751d007dd5b4
Ieskandar Zulqarnain <73181414+Zieszx@users.noreply.github.com>
2023-01-11 23:27:25 +0800

    Add files via upload

---
+++ src/test/java/com/glowterx/glowterx/GlowterxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glowterx.glowterx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlowterxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
